26c76bc25fc41daf956017289558bc61041d6ebc
Gabirel Angello <142102428+GabrielGotinha@users.noreply.github.com>
2024-01-16 21:58:26 -0300

    installdep

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home () {
                     
                 </div>
                 
-                <div className='flex justify-center '><Cardcont/></div>
+                <div className='flex justify-center'><Cardcont/></div>
                     
                 
             </main>
