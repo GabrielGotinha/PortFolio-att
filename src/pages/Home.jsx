cc8e874940da653bdf49129e2efaa1aee5601eb0
Gabirel Angello <142102428+GabrielGotinha@users.noreply.github.com>
2024-01-18 15:19:42 -0300

    2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
+import { Button } from 'react-bootstrap'
 import Cardcont from '../components/Cardcont'
 import { MaquinaDeEscrever } from '../components/EfeitoTy'
 import Footer from '../components/Footer'
 
 
+
 export default function Home () {
+    const onButtonClick = () => {
+        const pdfUrl = "/CurriculoAtt.pdf";
+        const link = document.createElement("a");
+        link.href = pdfUrl;
+        link.download = "CurriculoAtt.pdf"; // specify the filename
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
     return (
+        
         <>
             <main className='m-10  grid grid-rows-2 '>
                 <div className='flex justify-center '>
@@ -17,6 +29,10 @@ export default function Home () {
                 </div>
                 
                 <div className='flex justify-center'><Cardcont/></div>
+                <div className='flex justify-center mt-10'>
+                
+                    <Button variant="secondary" href='https://docs.google.com/document/d/1We2gl_jNNO-_MsffxhqTUTbL_0GyayqcU_3t9zUj3AY/edit?usp=sharing'>Curriculo</Button>
+                </div>
     
                 
 
